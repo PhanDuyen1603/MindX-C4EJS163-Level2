e3e5c492ddbed5481c005e77c3566f876d4bbdd6
May <22424005@student.hcmus.edu.vn>
2026-02-09 21:52:51 +0700

    update SS02

--- SS02/index.js
+++ SS02/index.js
@@ -1,6 +1,20 @@
-import { sumArray } from "./utils.js";
+import {sumArray, countOccurrences, removeDuplicates, flattenArray,isSymmetric} from './utils.js';
 
-const numbers = [1, 2, 3, 4];
-const result = sumArray(numbers);
+const myArray = [1, 3, 4, 3];
+const total = sumArray(myArray);
+console.log(`Tổng của mảng là: ${total}`);
 
-console.log(result); // 10
+const valueToCount = 3;
+const count = countOccurrences(myArray, valueToCount);
+console.log(`Giá trị ${valueToCount} xuất hiện ${count} lần trong mảng.`);
+
+const uniqueArray = removeDuplicates(myArray);
+console.log(`Mảng sau khi xóa phần tử trùng lặp: ${uniqueArray}`);
+
+const nestedArray = [[1, 2], [3, 4]];
+const flatArray = flattenArray(nestedArray);
+console.log(`Mảng sau khi làm phẳng: ${flatArray}`);
+
+const symmetricArray = [1, 2, 4, 3, 4, 2, 1];
+const isSym = isSymmetric(symmetricArray);
+console.log(`Mảng có đối xứng không? ${isSym}`);
--- SS02/utils.js
+++ SS02/utils.js
@@ -1,10 +1,57 @@
-// Bài 1: Tính tổng mảng
+//Bai 01: Tinh tong mang
 export function sumArray(arr) {
-  let sum = 0;          // Bước 1: tạo biến tổng
+    let sum = 0; //Buoc 1: tao bien tong
 
-  for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i]; // Bước 2: cộng từng phần tử
-  }
+    for (let i = 0; i < arr.length; i++) {
+       sum = sum + arr[i]; //Buoc 2: cong tung phan tu     
+    }
 
-  return sum;           // Bước 3: trả kết quả
+    return sum; //Buoc 3: tra ket qua
+}
+
+//Bai 2: 
+export function countOccurrences(arr, value) { // 2 1243242
+    let count = 0; //Buoc 1: tao bien dem
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === value) {
+            count++; //Buoc 2: tang bien dem neu phan tu bang gia tri can dem
+        }
+    }
+
+    return count; //Buoc 3: tra ket qua
+}
+
+//Bai 3:
+export function removeDuplicates(arr) {
+    let uniqueArr = []; //Buoc 1: tao mang chua phan tu khong trung lap
+
+    for(let i=0; i < arr.length; i++) { // 0 1 2 3 2 3 4 5
+        if(!uniqueArr.includes(arr[i])) { //Kiem tra phan tu arr[i] co trong uniqueArr chua
+            uniqueArr.push(arr[i]); 
+        }
+    }
+    return uniqueArr; //Buoc 3: tra ket qua
+}
+
+// bai 4: tinh tong cac so le trong mang
+export function flattenArray(arr) {
+    let flatArr = []; //Buoc 1: tao mang rong de chua phan tu sau khi lam phang
+    
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[i].length; j++) {
+            flatArr.push(arr[i][j]); // Buoc 2: them phan tu vao flatArr. // arr[0]
+        }
+    }
+    return flatArr; // Buoc 3: tra ket qua
+}
+
+//bai 5: isSymmetric
+export function isSymmetric(arr) {
+   for(let i = 0; i < arr.length / 2; i++) {
+        if(arr[i] !== arr[arr.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
